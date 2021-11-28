7135210c63c1222c86445e04176044a8cd104fe8
laurentfrederick <laurent.frederick@icloud.com>
2021-11-28 23:33:20 +0100

    add ft_itoa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lfrederi <lfrederi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 10:02:19 by lfrederi          #+#    #+#             */
-/*   Updated: 2021/11/28 09:50:05 by lfrederi         ###   ########.fr       */
+/*   Updated: 2021/11/28 22:45:14 by lfrederi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 # endif
