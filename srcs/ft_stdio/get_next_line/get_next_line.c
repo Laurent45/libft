bda01f60d55a2e83121aac5636aceabba8dff3d9
Esquanor_linux <outsider973@outlook.fr>
2022-04-13 22:35:06 +0200

    fix bug gnl ft_strlen

--- srcs/ft_stdio/get_next_line/get_next_line.c
+++ srcs/ft_stdio/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lfrederi <lfrederi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 12:44:32 by lfrederi          #+#    #+#             */
-/*   Updated: 2022/02/04 14:13:52 by lfrederi         ###   ########.fr       */
+/*   Updated: 2022/04/13 22:34:35 by lfrederi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ char	*ft_createline(int fd, char *curr)
 	buf = ft_getbuffer(buffd, fd);
 	if (!buf)
 		return (NULL);
-	if (!ft_strlen(buf))
+	if (!ft_strlen_gnl(buf))
 		if (read(fd, buf, BUFFER_SIZE) <= 0)
 			return (NULL);
 	size_copy = ft_sizetocpy(buf);
-	lencurr = ft_strlen(curr);
+	lencurr = ft_strlen_gnl(curr);
 	ret = (char *) malloc(sizeof(char) * (lencurr + size_copy + 1));
 	if (!ret)
 		return (NULL);
@@ -39,13 +39,13 @@ char	*ft_createline(int fd, char *curr)
 	if (curr)
 		free(curr);
 	ft_strlcat(ret, buf, size_copy);
-	ft_managebuffer(buf, ft_strlen(buf), size_copy);
+	ft_managebuffer(buf, ft_strlen_gnl(buf), size_copy);
 	return (ret);
 }
 
 int	ft_isnewline(char *str)
 {
-	return (str[ft_strlen(str) - 1] == '\n');
+	return (str[ft_strlen_gnl(str) - 1] == '\n');
 }
 
 char	*get_next_line(int fd)
