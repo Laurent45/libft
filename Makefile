918c786105f6bbf99d7456bad9b7733f87b86551
laurentfrederick <laurent.frederick@icloud.com>
2022-05-31 09:48:28 +0200

    add ft_clear_split.c and check norminette

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ SRCS	= 	$(addprefix srcs/ft_ctype/, ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_is
 										ft_toupper.c ft_tolower.c\
 										ft_strchr.c ft_strrchr.c ft_strncmp.c ft_strcmp.c ft_strnstr.c\
 										ft_atoi.c ft_itoa.c\
-										ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c\
+										ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c ft_clear_split.c\
 										ft_strmapi.c ft_striteri.c)\
 			$(addprefix srcs/ft_stdio/, ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c\
 										get_next_line/get_next_line.c get_next_line/get_next_line_utils.c\
--- headers/ft_string.h
+++ headers/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: lfrederi <lfrederi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 16:37:01 by lfrederi          #+#    #+#             */
-/*   Updated: 2022/05/25 14:23:58 by lfrederi         ###   ########.fr       */
+/*   Updated: 2022/05/31 09:43:10 by lfrederi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,9 +96,11 @@ char	*ft_strdup(const char *s1);
  */
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
+char	**ft_split(char const *s, char c);
+void	ft_clear_split(char **split);
+
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
